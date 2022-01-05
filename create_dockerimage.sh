db89f13f2803c2a4dcf165b3493ba51bc7ff8f4a
Thomas Schmitt <thomas@linuxmuster.net>
2022-01-05 17:36:22 +0100

    Fix #1, groupadd: GID 0 already exists.

--- create_dockerimage.sh
+++ create_dockerimage.sh
@@ -3,12 +3,17 @@
 # create docker image
 #
 # thomas@linuxmuster.net
-# 20210511
+# 20220105
 #
 
 MY_NAME="linbo-build-docker"
 MY_UID="$(id -u)"
 MY_GID="$(id -g)"
+
+if [ "$MY_UID" = "O" ]; then
+  echo "Run this script as an unprivileged user not as root! The user has to be member of the group \"docker\"."
+  exit 1
+fi
 
 # sets uid/gid in Dockerfile
 sed -i "s|^ENV MY_UID .*|ENV MY_UID $MY_UID|" Dockerfile
