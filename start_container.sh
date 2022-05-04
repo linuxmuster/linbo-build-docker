2b690d5f9116bf34b6e8f09d6d649503b2f87df7
Thomas Schmitt <thomas@linuxmuster.net>
2022-05-04 12:16:50 +0000

    added missing start_container.sh for 18.04.

--- start_container.sh
+++ start_container.sh
@@ -3,11 +3,11 @@
 # starts container in interactive mode
 #
 # thomas@linuxmuster.net
-# 20210511
+# 20220426
 #
 
 MY_DIR="$(pwd)"
-MY_NAME="linbo-build-docker"
+MY_NAME="linbo-build-docker-1804"
 MY_UID="$(id -u)"
 MY_GID="$(id -g)"
 MY_USER="linbo"
